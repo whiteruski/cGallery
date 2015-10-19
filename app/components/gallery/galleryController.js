bebc7023f04cbddfc3f65b40afb6cfd39584c9ab
Sushiii <sushiii@sushiii.sushiii>
2015-10-18 22:11:14 -0500

    console.log

--- app/components/gallery/galleryController.js
+++ app/components/gallery/galleryController.js
@@ -3,8 +3,6 @@ angular.module('cGalleryApp.controllers', [])
 
     GalleryService.getEntries().then(function(data) {
         $scope.entries = data.data.items;
-        console.log("Entries:");
-        console.log(data.data.items);
     });
 
     $scope.showFile = function(id) {
